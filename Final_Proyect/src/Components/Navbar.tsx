b1cc3415a4597c06dcfbd79592bda7cde2db15d3
AM1120 <ailenroa1149@gmail.com>
2024-10-24 17:56:32 -0400

    Proyecto_Final

---
+++ Final_Proyect/src/Components/Navbar.tsx
@@ -0,0 +1,80 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { UserState } from "../Redux/Slices/UserSlices";
+import { logout } from "../Redux/Slices/UserSlices";
+import { useDispatch } from "react-redux";
+
+export default function Navbar() {
+    const dispatch = useDispatch(); 
+
+    const user = useSelector((state:{user:UserState}) => state.user);
+
+
+    return (
+        <nav className="min-h-[10vh] max-h-[10vh] flex flex-row justify-between items-center bg-white px-6 shadow-md">
+            <Link to="/" className="text-2xl font-bold text-black hover:text-blue-700 transition-colors">
+                Inicio
+            </Link>
+            <ul className="flex flex-row gap-6">
+                {user.isLoggedIn && user.userSession?.t_rol==2&&(
+                <li>
+                    <Link
+                        to="/crear-producto"
+                        className="text-lg text-blue-500 hover:text-blue-600 transition-colors"
+                    > Crear Producto
+                    </Link>
+                </li>
+                )}
+
+                <li>
+                    <Link
+                        to="/catalogo"
+                        className="text-lg text-blue-500 hover:text-blue-600 transition-colors"
+                    >
+                        Catálogo
+                    </Link>
+                </li>
+    
+                {
+                    user.isLoggedIn ? (
+                        <li>
+
+                            <Link
+                                to={"/"}
+                                className="text-lg text-blue-500 hover:text-blue-600 transition-colors"
+                                onClick={()=>dispatch(logout())}
+                                >
+                                Cerrar Sesión
+                            </Link>
+                        </li>
+
+                    ) : (
+                        <>
+                        <li>
+                            <Link
+                                to="/registrarse"
+                                className="text-lg text-blue-500 hover:text-blue-600 transition-colors"
+                            >
+                                Registrarse
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/login"
+                                className="text-lg text-blue-500 hover:text-blue-600 transition-colors"
+                            >
+                                Iniciar Sesión
+                            </Link>
+
+                        </li>
+
+                        </>
+
+                    )
+                }
+
+
+            </ul>
+        </nav>
+    );
+}
